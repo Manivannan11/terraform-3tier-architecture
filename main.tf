2d96ccb4ea4fa26720d74ba62c1144b8e3e158cc
manivannan11 <mallai.mani1112@gmail.com>
2024-02-27 22:12:20 +0530

    adding security group child module

---
+++ main.tf
@@ -0,0 +1,3 @@
+module "aws_security_group" {
+  source = "github.com/manivannan11/terraform-aws-security-group"
+}
---
+++ provider.tf
@@ -0,0 +1,5 @@
+provider "aws" {
+  region                  = "us-east-1"
+  access_key              = "AKIA35UDAXJXN6ZFBNPR"
+  secret_key              = "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"
+}
