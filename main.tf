7e54bc5d0ba8f4e6d3a524249208bad66b8533ef
manivannan11 <mallai.mani1112@gmail.com>
2024-02-27 22:48:39 +0530

    made few changes

--- main.tf
+++ main.tf
@@ -1,3 +1,11 @@
 module "aws_security_group" {
   source = "github.com/manivannan11/terraform-aws-security-group"
 }
+
+module "create_sg" {
+  source = "github.com/manivannan11/terraform-aws-security-group"
+
+  security_group_name        = "example-security-group"
+  security_group_description = "Example security group for web servers"
+  vpc_id                     = "vpc-028aa094833effbc8" # Replace with the ID of your existing VPC
+}
